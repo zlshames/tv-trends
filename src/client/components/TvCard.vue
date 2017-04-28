37220cf879db15aadd3d38e2064c225f63d5a73f
Zach Shames <zlshames@gmail.com>
2017-04-27 20:11:05 -0400

    Added ratings by season && toggling

--- src/client/components/TvCard.vue
+++ src/client/components/TvCard.vue
@@ -19,6 +19,12 @@
       </div>
     </div>
     <div v-if="displayTrends" class="card-body">
+      <button v-if="view === 'bySeason'" @click="getDataByEpisode" type="button" class="el-button el-button--default">
+        Load By Episode
+      </button>
+      <button v-if="view === 'byEpisode'" @click="getDataBySeason" type="button" class="el-button el-button--default">
+        Load By Season
+      </button>
       <vue-chart v-if="chartData.rows.length > 0" class="chart" :columns="chartData.columns" :rows="chartData.rows" :options="chartData.options" />
       <p v-if="chartData.rows.length === 0 && !loading">No trends available for this title</p>
       <p v-if="chartData.rows.length === 0 && loading">Loading...</p>
@@ -31,6 +37,7 @@
     props: ['item', 'displayTrends', 'toggleTrends'],
     data() {
       return {
+        view: 'bySeason',
         loading: false,
         chartData: {
           columns: [
@@ -62,7 +69,8 @@
     watch: {
       displayTrends() {
         if (this.displayTrends) {
-          this.getDataByEpisode()
+          //this.getDataByEpisode()
+          this.getDataBySeason()
         }
       }
     },
@@ -97,6 +105,7 @@
       },
       async getDataByEpisode() {
         this.loading = true
+        this.view = 'byEpisode'
 
         const episodes = await this.fetchEpisodes()
         const newData = {
@@ -137,12 +146,90 @@
 
         this.chartData = newData
         this.loading = false
+      },
+      async getDataBySeason() {
+        this.loading = true
+        this.view = 'bySeason'
+
+        const episodes = await this.fetchEpisodes()
+        const newData = {
+          columns: [
+            {
+              'type': 'string',
+              'label': 'Episode'
+            }
+          ],
+          rows: [],
+          options: {
+            title: 'Rating by Season',
+            hAxis: {
+                title: 'Episode'
+            },
+            vAxis: {
+                title: 'Rating'
+            },
+            width: 600,
+            height: 400,
+            backgroundColor: 'transparent'
+          }
+        }
+
+        // Get seasons for columns
+        for (let i = 0; i < this.item.number_of_seasons; i++) {
+          newData.columns.push({
+            'type': 'number',
+            'label': 'Season ' + (i + 1)
+          })
+        }
+
+        // Group epidoes into seasons
+        const seasons = []
+        for (let i = 0; i < episodes.length; i++) {
+          if (seasons.length < episodes[i].season_number) {
+            seasons.push([])
+          }
+
+          seasons[episodes[i].season_number - 1].push(episodes[i])
+        }
+
+        // Get most episodes in a season
+        let most = 0
+        for (let i = 0; i < seasons.length; i++) {
+          if (seasons[i].length > most) {
+            most = seasons[i].length
+          }
+        }
+
+        // Group by Episode
+        for (let i = 0; i < most; i++) {
+          let data = [`Episode ${i}`]
+
+          for (let x = 0; x < seasons.length; x++) {
+            if (seasons[x][i]) {
+              data.push(seasons[x][i].vote_average)
+            } else {
+              data.push(NaN)
+            }
+          }
+
+          newData.rows.push(data)
+        }
+
+        this.chartData = newData
+        this.loading = false
       }
     }
   }
 </script>
 
 <style scoped>
+  .card-body > .el-button {
+    height: 20px;
+    padding-top: 3px;
+    margin-left: 10px;
+    margin-top: 10px;
+  }
+
   .card {
     border: 1px solid #d1dbe5;
     border-radius: 4px;
@@ -208,9 +295,10 @@
     border-top: 1px solid #d1dbe5;
     margin-top: 15px;
     width: 100%;
-    height: 400px;
+    height: 450px;
 
     display: flex;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
   }
